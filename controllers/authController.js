823d621d1ec07018b0d4afb9c332863b916ba416
Om Chavada <142745673+Om005@users.noreply.github.com>
2025-05-19 20:00:00 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,8 +23,8 @@ export const register = async(req, res)=>{
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production'?'none':'strict',
+            secure: true,
+            sameSite: "None",
             maxAge: 7*24*60*60*1000
         })
         const mailOptions ={
@@ -86,8 +86,8 @@ export const logout = async(req, res)=>{
     try{
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production'?'none':'strict',
+            secure: true,
+            sameSite: "None",
         })
         return res.json({success: true, message: 'Logged out'})
     }
