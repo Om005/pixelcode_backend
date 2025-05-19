863bc2111121dcd857dc57179e05efc9f5ec19c9
Om Chavada <142745673+Om005@users.noreply.github.com>
2025-05-19 19:54:39 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,8 +69,8 @@ if(!email || !password){
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production'?'none':'strict',
+            secure: true,
+            sameSite: "None",
             maxAge: 7*24*60*60*1000
         })
         
